7599535271f8c80ca8d955adab5f1083311e2d6b
Mutaz Ismael <mutaz@umich.edu>
2026-05-20 01:23:47 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+variable = 1
+
+print(type(variable))
+variable = 1.2
+
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = "hi"
+
+print(type(variable))
+variable = [1,2]
+
+print(type(variable))
